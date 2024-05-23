1d93fe62544f62655a298237f5016bd6a53a40c2
Priya <priya.koruche@globant.com>
2024-05-23 15:14:50 +0530

    updated base url

--- src/api.js
+++ src/api.js
@@ -3,8 +3,9 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Create an axios instance with the base URL
 const api = axios.create({
-  baseURL: 'http://192.168.31.242:3005/', // Replace with your actual base URL
+  // baseURL: 'http://192.168.31.242:3005/', // Replace with your actual base URL
   // baseURL: 'http://10.221.0.79:3005/',
+  baseURL: 'http://localhost:3005/',
 });
 
 // Add a request interceptor to include the authorization token
